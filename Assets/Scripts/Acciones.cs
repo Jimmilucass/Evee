8fe5c0cc20d0c49e2a3d1a8752e3923e042ce052
Jimmy A. Pilatasig <ejpilatasig@saludsa.com.ec>
2025-08-05 16:02:48 -0500

    proyecto evee

---
+++ Assets/Scripts/Acciones.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Acciones : MonoBehaviour
+{
+    //variables iniciar pausar
+    public Button btn_no;
+    public bool estado = true;
+    public Sprite si, no;
+
+    //variables prefabs
+    public GameObject letreroSi;
+    public GameObject cupon;
+
+    private GameObject objetoSi;
+    private GameObject objetoIncognita;
+
+    public void AbrirSi()
+    {
+        objetoSi = Instantiate(letreroSi);
+    }
+
+    public void EveSiNo()
+    {
+        if (estado.Equals(true))
+        {
+            //cambiar a estado si
+            btn_no.GetComponent<Image>().sprite = si;      
+
+            estado = !estado;
+        }
+        else
+        {
+            //cargar letrero si una vez presionado el boton no
+            AbrirSi();
+        }
+        /*else
+        {
+            //cambiar  a estado si
+            btn_no.GetComponent<Image>().sprite = no;
+            
+            estado = !estado;
+        }*/
+    }
+    public void AbrirIncognita()
+    {
+        objetoIncognita = Instantiate(cupon);
+    }
+
+
+}
---
+++ Assets/Scripts/AccionesCupon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccionesCupon : MonoBehaviour
+{
+    //variables
+    private GameObject obj;
+
+    //accion cerrar cuadro de informacion
+    public void CCerrar()
+    {
+        obj = GameObject.Find("CanvasCupon(Clone)");
+        GameObject.Destroy(obj);
+    }
+
+}
