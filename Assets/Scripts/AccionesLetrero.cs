d44877a0f9156b262273f8af1633b62611acae27
Jimmilucass <alexder.j.p@gmail.com>
2025-12-29 08:52:18 -0500

    Evee proyecto inicial

---
+++ Assets/Scripts/AccionesLetrero.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccionesLetrero : MonoBehaviour
+{
+    //variables
+    private GameObject obj;
+
+    //accion cerrar cuadro de informacion
+    public void LCerrar()
+    {
+        obj = GameObject.Find("CanvasLetrero(Clone)");
+        GameObject.Destroy(obj);
+    }
+
+}
